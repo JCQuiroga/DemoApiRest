4a94a01ffbd7ca484c8a526e23c44300e6741312
Jose C Quiroga <josecquiroga@gmail.com>
2016-02-17 11:13:51 +0100

    -[:·:]-

--- DemoApiRest/App_Start/WebApiConfig.cs
+++ DemoApiRest/App_Start/WebApiConfig.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using System.Web.Http.OData.Extensions;
 using DemoApiRest.Extensiones;
+using DemoApiRest.Models;
 
 namespace DemoApiRest
 {
     public static class WebApiConfig
     {
+        //faltan metodos.
+
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
             config.MessageHandlers.Add(new Loghandler());
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            //Copiamos esto del controller con OData que hemos creado.
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Usuario>("Usuarios");
+            builder.EntitySet<Mensaje>("Mensaje");
+
+            config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
