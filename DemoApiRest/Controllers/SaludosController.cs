6db75d28da122b11ebc59dc28a9404b14e1cc266
Jose C Quiroga <josecquiroga@gmail.com>
2016-02-17 10:20:24 +0100

    -][-

---
+++ DemoApiRest/Controllers/SaludosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace DemoApiRest.Controllers
+{
+    public class SaludosController : ApiController
+    {
+
+        //get saludos
+        [ResponseType(typeof (String))]
+        public IHttpActionResult Get(String nombre)
+        {
+            return Ok($"Hola {nombre}");
+        }
+    }
+}
---
+++ DemoApiRest/Extensiones/Loghandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoApiRest.Extensiones
+{
+    public class Loghandler:DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancelationToken)
+        {
+            using (var d = File.AppendText(@"c:\log\logapi.txt"))
+            {
+                d.WriteLine("{o:d} Request ->{1}",DateTime.Now,request);
+            }
+            return base.SendAsync(request, cancelationToken);
+        }
+    }
+}
